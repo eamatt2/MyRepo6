05a5ff61b2519ce62d5237d6db7b592ea6bdb34c
eamatt2 <ematthewfamily@gmail.com>
2023-02-23 18:23:53 -0500

    finished lab 6

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
-int search(int numbers[], int low, int high, int value) 
+int search(int numbers[], int low, int high, int value)
 {
-	return -1;
+    if(low>high){
+        return -1; //value won't be in array between low and high
+    }
+	int mid = (low + high)/2;
+	if(numbers[mid] == value){
+        return mid;
+	}
+	else if(numbers[mid] > value){
+        return search(numbers, low, mid - 1, value);
+	}
+	else {
+        return search(numbers, mid + 1, high, value);
+	}
 }
 
 void printArray(int numbers[], int sz)
@@ -28,7 +40,7 @@ int main(void)
 	FILE* inFile = fopen("input.txt","r");
 
 	fscanf(inFile, " %d\n", &numInputs);
-	
+
 	while (numInputs-- > 0)
 	{
 		fscanf(inFile, " %d\n", &countOfNums);
